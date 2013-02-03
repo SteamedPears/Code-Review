d53e2e33e2f653ad60e1b36be582f539452beb42
Simon David Pratt <me@SimonDavidPratt.com>
2013-02-03 14:39:43 -0500

    Wrote install instructions, empty deploy scripts
    	new file:   INSTALL
    	new file:   bin/start_dev.sh
    	new file:   bin/stop_dev.sh
    	new file:   bin/update_server.sh

---
+++ bin/start_dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO: make this start the dev server on port 8080
+# TODO: save pid to file for stopping
+
+echo Not yet implemented.  --sdp
---
+++ bin/stop_dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO: read pid from file
+# TODO: stop the dev server
+
+echo Not yet implemented.  --sdp
