04b477a11e8628c1490317ed6b88e0548cca84f8
Peter Simonyi <peter@steamedpears.com>
2013-03-10 17:39:57 -0400

    Normalized indentation; added vim modelines
    	modified:   src/bookmarklet/index.html
    	modified:   src/server/languageList.js
    	modified:   src/server/models/code.js
    	modified:   src/server/models/comment.js
    	modified:   src/server/models/test_data.js
    	modified:   src/server/package.json
    	modified:   src/server/proxy.js
    	modified:   src/server/requestHandlers.js
    	modified:   src/server/router.js
    	modified:   src/server/server.js
    	modified:   src/server/static-server.js

--- src/server/models/code.js
+++ src/server/models/code.js
@@ -3,31 +3,33 @@
 ******************************************************************************/
 
 module.exports = function(sequelize, DataTypes) {
-	var Code = sequelize.define('Code',{
-	    uuid: {
-		    type: DataTypes.STRING,
-		    primaryKey: true,
-		    notNull: true,
-		    notEmpty: true
-	    },
-	    text: {
-		    type: DataTypes.TEXT,
-		    validate: {
-		        notNull: true,
-		        notEmpty: true
-		    }
-	    },
-	    lang: {
-		    type: DataTypes.STRING,
-		    validate: {
-		        notNull: true,
-		        notEmpty: true,
-		    }
-	    }
-	},{
-	    // don't pluralize code
-	    freezeTableName: true
-	});
+  var Code = sequelize.define('Code',{
+    uuid: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      notNull: true,
+      notEmpty: true
+    },
+    text: {
+      type: DataTypes.TEXT,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    lang: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+      }
+    }
+  },{
+    // don't pluralize code
+    freezeTableName: true
+  });
 
-	return Code;
+  return Code;
 };
+
+/* vim: set softtabstop=2 shiftwidth=2 tabstop=8 expandtab textwidth=80: */
