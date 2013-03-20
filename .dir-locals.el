1c25b107f3f554143cca855853d36e36592db016
Simon Pratt <simon@pr4tt.com>
2013-03-20 12:49:22 -0400

    Now using tabs in emacs makefile-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ((nil . ((tab-width . 8)
 		 (indent-tabs-mode . nil)
 		 (fill-column . 80)))
- (espresso-mode . ((espresso-indent-level . 2))))
+ (espresso-mode . ((espresso-indent-level . 2)))
+ (makefile-mode . ((indent-tabs-mode . t))))
