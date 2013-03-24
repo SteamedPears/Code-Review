1ba7ad49f112bcfed01727771d3b30868d88be68
Simon Pratt <simon@pr4tt.com>
2013-03-24 12:42:13 -0400

    Reset now uses git clean

--- bin/reset_dev.sh
+++ bin/reset_dev.sh
@@ -8,27 +8,5 @@
 # This script should reset the state of the development environment.
 ######################################################################
 
-######################################################################
-# Configuration
-
-DB_INFO_FILE="models/db_info.js"
-DEV_DB="models/data.db"
-NODE_MODULES="node_modules"
-
-######################################################################
-# kill the server if running
 bin/stop_dev.sh 2> /dev/null
-
-######################################################################
-# reset the client side
-bin/reset_client.sh
-
-######################################################################
-# delete metadata and data so db rebuilds next start
-cd src/server
-rm -f $DB_INFO_FILE
-rm -f $DEV_DB
-
-######################################################################
-# delete node modules so modules reinstalled next start
-rm -rf $NODE_MODULES
+git clean -qxdff
