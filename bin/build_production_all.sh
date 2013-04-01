53e02365b268816e2cd9fb4f95c494ff325834fc
Christian Delahousse <christian@delahousse.ca>
2013-04-01 12:44:46 -0400

    cp -r to -R for BSD compatibility

--- bin/build_production_all.sh
+++ bin/build_production_all.sh
@@ -67,8 +67,8 @@ echo "Optimizing client using r.js, saving to `basename $TARGET_TMP`"
 cd "$CLIENT_DIR"; node "$RJS" -o "$RJS_BUILD_PROFILE" dir="$TARGET_TMP"
 
 echo "Copying important client files to `basename $TARGET`"
-cp -avr "$TARGET_TMP/scripts"  "$TARGET/scripts"
-cp -avr "$TARGET_TMP/styles"  "$TARGET/styles"
+cp -avR "$TARGET_TMP/scripts"  "$TARGET/scripts"
+cp -avR "$TARGET_TMP/styles"  "$TARGET/styles"
 cp -av "$TARGET_TMP/index.html"  "$TARGET/index.html"
 
 echo "Resetting Bookmarklet"
