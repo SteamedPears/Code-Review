b5abb68e0e297b238c18c1d79588cd08a4a6d3b7
Bheesham Persaud <bheesham123@gmail.com>
2013-04-09 01:53:40 +0000

    Now installs to $PREFIX/bleh, doesn't handle copying client stuff.
    	new file:   bin/.install_server.sh.swp
    	deleted:    bin/exe/.folder
    	modified:   bin/install_server.sh
    	modified:   etc/server_devel.yml
    	modified:   etc/server_prod.yml

--- bin/install_server.sh
+++ bin/install_server.sh
@@ -5,8 +5,9 @@
 # Project: Code Review
 # By:      Steamed Pears
 #
-# This script should install nodejs, and use the default configuration
-# for npm.
+# Downloads and installs:
+#   - nodejs and npm
+#   - redis-server
 ######################################################################
 
 ROOT_DIR=`pwd`
@@ -15,52 +16,64 @@ ROOT_DIR=`pwd`
 # Configuration
 ######################################################################
 
-CLIENT_TARGET_DIR=~/webapps/cr_front
-
-BIN_DIR=$ROOT_DIR/bin/exe
-
 # A valid node version
 NODE_VERSION=0.10.2
 
 # Can be either 64 or 86
 NODE_ARCH=64
 
+# Redis version
 REDIS_VERSION=2.6.12
 
 ######################################################################
 
 NODE_F=node-v$NODE_VERSION-linux-x$NODE_ARCH
-NODE_BINS=$BIN_DIR/node/bin
+NODE_BIN=/tmp/node/bin/node
+NPM_BIN=/tmp/node/bin/npm
 
 REDIS_F=redis-$REDIS_VERSION
+REDIS_BIN=/tmp/redis/src/redis-server
 
-cd $BIN_DIR
+cd /tmp
+
+######################################################################
+# nodejs and npm
+######################################################################
 
 echo Installing component: node.js
 
 echo - downloading...
-wget -q http://nodejs.org/dist/v$NODE_VERSION/$NODE_F.tar.gz
+wget -O /tmp/node.tar.gz -q http://nodejs.org/dist/v$NODE_VERSION/$NODE_F.tar.gz
 
 echo - extracting...
-tar -xf $NODE_F.tar.gz -C $BIN_DIR/
-rm $NODE_F.tar.gz
-mv -f $NODE_F node
+tar -xf node.tar.gz
+
+echo - installing...
+mkdir -p $PREFIX/usr/bin
+cp /tmp/$NODE_F/bin/node $PREFIX/usr/bin/node
+cp /tmp/$NODE_F/bin/npm $PREFIX/usr/bin/npm
 
 echo - complete!
+
+######################################################################
+# redis-server
+######################################################################
 
 echo Installing component: redis
 
 echo - downloading...
-wget -q http://redis.googlecode.com/files/$REDIS_F.tar.gz
+wget -O redis.tar.gz -q http://redis.googlecode.com/files/$REDIS_F.tar.gz
 
-echo Download complete, extracing redis
-tar -xf $REDIS_F.tar.gz -C $BIN_DIR/
-rm $REDIS_F.tar.gz
-mv -f $REDIS_F redis
-cd $BIN_DIR/redis 
+echo - extracting redis...
+tar -xf redis.tar.gz
+cd /tmp/$REDIS_F 
 
-echo Compiling redis
+echo - compiling redis...
 make
 
-echo Copying client files into the specified directory 
-cp -R $ROOT_DIR/src/client/* $CLIENT_TARGET_DIR/
+echo - installing redis...
+cp /tmp/$REDIS_F/src/redis-server $PREFIX/usr/bin/redis-server
+
+echo - complete!
+
+echo The installation has been successful
